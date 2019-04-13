64feda08ea3905ac01dbf68a95c19da1b2cb351e
user <bob@ubuntu.ubuntu-domain>
2019-04-13 16:09:49 +0200

    add check_rule(checked), add check_full(checked), update success(unchecked)

--- workspace/script/Panel.py
+++ workspace/script/Panel.py
@@ -33,30 +33,77 @@ class RenjuPanel:
             else:
                 pose = rob2.run()
                 self.set_pose(pose, -1, 'O')
+            obey_rule = self.check_rule()
+            assert obey_rule, 'someone break the rule'
             self.draw()
-            self.check_success()
+            success = self.check_success()
+            if success:
+                break
+            isfull = self.check_full()
+            if isfull:
+                break
+
+    def get_check_wnd(self):
+        x, y = self.pose
+        x1 = max(0, x-4)
+        y1 = max(0, y-4)
+        x2 = min(self.scale-1, x+4)
+        y2 = min(self.scale-1, y+4)
+        self.check_wnd = self.stat[x1:x2+1, y1:y2+1]
+        return self.check_wnd
 
     def check_success(self):
-        check_matrix1 = np.ones((5, 5))
-        top_left_x = max(0, self.pose[0]-4)
-        top_left_y = max(0, self.pose[1]-4)
-        bottom_right_x = min(self.scale-1, self.pose[0]+4)
-        bottom_right_y = min(self.scale-1, self.pose[1]+4)
-        print(top_left_x, top_left_y, bottom_right_x, bottom_right_y)
+        check_mat1 = np.ones((1, 5))
+        check_mat2 = np.ones((5, 1))
+        check_mat3 = np.eye(5)
+        check_wnd = self.get_check_wnd()
+        length = len(check_wnd)
+        for i in range(length-4):
+            mat1 = check_wnd[i:i+5, i:i+5]
+            mat2 = np.rot90(mat1)
+            tmp = np.dot(check_mat1, mat1)
+            if -5 in tmp.tolist() or 5 in tmp.tolist():
+                return True
+            tmp = np.dot(mat1, check_mat2)
+            if -5 in tmp.tolist() or 5 in tmp.tolist():
+                return True
+            tmp = (mat1*check_mat3).sum()
+            if tmp == 5 or tmp == -5:
+                return True
+            tmp = (mat2*check_mat3).sum()
+            if tmp == 5 or tmp == -5:
+                return True
+            i += 1
+        return False
 
-        pass
+    def check_rule(self):
+        if self.stat.sum() in [0, 1]:
+            return True
+        else:
+            return False
+
+    def check_full(self):
+        if 0 in self.stat.reshape(1, -1).tolist()[0]:
+            return True
+        else:
+            return False
 
     def set_pose(self, pose, value, signal):
         self.pose = pose
         self.frame[pose] = signal
+        assert self.stat[pose] == 0, 'wrong pose is given'
         self.stat[pose] = value
+
 
 
 if __name__ == '__main__':
     renju = RenjuPanel(17)
     renju.set_pose((6, 6), -1, 'O')
+    renju.set_pose((5, 5), -1, 'O')
+    renju.set_pose((4, 4), -1, 'O')
+    renju.set_pose((3, 3), -1, 'O')
+    renju.set_pose((2, 2), -1, 'O')
     renju.draw()
-    renju.check_success()
     #renju.loop()
 
 
